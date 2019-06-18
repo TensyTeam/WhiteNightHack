dd222ae3ed5c5b398b5df426b68a23197376dd6d
Vladislav Evtushenko <vladislav.evtushenko@kraftvaerk.com>
2019-06-18 18:47:52 +0300

    added requesting qr code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import io from "socket.io-client";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const backendUrl = "https://e94dc841.ngrok.io";
+
+export const getQrCode = (socketName, query) => {
+    const socket = io(`${backendUrl}/${socketName}/`, {
+        forceNew: true,
+        query
+    });
+    return new Promise(resolve =>
+        socket.on("qrCode", ({ qrCode, identifier }) => resolve({ qrCode, socket, identifier }))
+    );
+};
+
+const f = async() => {
+    const { qrCode: ssoQrCode, socket, identifier } = await getQrCode("authenticate");
+    ReactDOM.render(<App />, document.getElementById("root"));
+}
+
+f();
+
+
+
