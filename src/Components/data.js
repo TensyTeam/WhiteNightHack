8ba988cf6dffdf736170dab2eb50046f6076b5bb
mike-petrov <mis5116@yandex.ru>
2019-06-18 21:48:48 +0300

    areas data

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,17 +1,57 @@
 export const data = {
-    'type': 'Feature',
-    'geometry': {
-        'type': 'Polygon',
-        "properties": {
-            "name": 'Name',
-        },
-        'coordinates': [
-            [
-                [30.347522,59.915340],
-                [30.350097,59.914953],
-                [30.348316,59.912124],
-                [30.345677,59.912549]
-            ]
-        ]
-    }
+    "type": "FeatureCollection",
+    "features": [
+        {
+            'type': 'Feature',
+            "properties": {
+                "name": '~5 min'
+            },
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            30.347522, 59.915340
+                        ],
+                        [
+                            30.350097, 59.914953
+                        ],
+                        [
+                            30.348316, 59.912124
+                        ],
+                        [
+                            30.345677, 59.912549
+                        ]
+                    ]
+                ]
+            }
+        }, {
+            'type': 'Feature',
+            "properties": {
+                "name": '~2 min'
+            },
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                    [
+                        [
+                            30.340816, 59.910849
+                        ],
+                        [
+                            30.343884, 59.910284
+                        ],
+                        [
+                            30.342478, 59.908412
+                        ],
+                        [
+                            30.339506, 59.909176
+                        ],
+                        [
+                            30.340816, 59.910849
+                        ]
+                    ]
+                ]
+            }
+        }
+    ]
 }
